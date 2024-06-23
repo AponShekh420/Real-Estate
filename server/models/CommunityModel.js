472b7ac2f69c161f327534ddc4e1e02a83f61525
Apon Shekh <aponshekh420@gmail.com>
2024-06-23 17:10:25 +0600

    trying to create model of community

---
+++ server/models/CommunityModel.js
@@ -0,0 +1,98 @@
+const mongoose = require("mongoose");
+
+const CommunitySchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  website: String,
+  phone: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: mongoose.Types.ObjectId,
+    required: true
+  },
+  city: {
+    type: mongoose.Types.ObjectId,
+    required: true
+  },
+  area: {
+    type: mongoose.Types.ObjectId,
+    required: true
+  },
+  zip: String,
+  minPrice: {
+    type: Number,
+    required: true
+  },
+  maxPrice: {
+    type: Number,
+    required: true
+  },
+  homeTypes: [
+    {
+      type: String,
+      required: true
+    }
+  ],
+  communitySize: {
+    type: Number,
+    required: true
+  },
+  ageRestrictions: {
+    type: Boolean,
+    required: true
+  },
+  gated: {
+    type: Boolean,
+    required: true
+  },
+  builtStart: {
+    type: String,
+    required: true
+  },
+  builtEnd: {
+    type: String,
+    required: true
+  },
+  Overview: {
+    type: String
+  },
+  imgs: [
+    {
+      type: String,
+      required: true
+    }
+  ],
+  bedrooms: {
+    type: Number,
+    default: 0
+  },
+  bathrooms: {
+    type: Number,
+    default: 0
+  },
+  Garages: {
+    type: Number,
+    default: 0
+  },
+  active: {
+    type: Boolean,
+    default: true
+  },
+  listIn: {
+    type: String,
+    required: true
+  }
+});
+
+
+const CommunityModel = mongoose.models.Community || mongoose.model("Community", CommunitySchema);
+
+module.exports = CommunityModel;
