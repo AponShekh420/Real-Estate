c52e3929e4bd8784439e2bd16c1c13c2f4957d1a
Apon Shekh <aponshekh420@gmail.com>
2024-06-25 20:20:18 +0600

    i did updated the city delete route, but have some work rest to do

---
+++ server/controllers/cityController/deleteCity.js
@@ -0,0 +1,70 @@
+// import models
+const AreaModel = require('../../models/AreaModel');
+const CityModel = require('../../models/CityModel');
+const StateModel = require('../../models/StateModel')
+const CommunityModel = require('../../models/CommunityModel')
+
+
+const deleteCity = async (req, res) => {
+  const {cityId} = req.body
+
+  try {
+    const cityDeleteStatus = await CityModel.findByIdAndDelete(cityId);
+
+    // check: if the state has deleted successfully then we should remove this stateId from city, area, and communtiy and make the city, area and community deactive
+    if(cityDeleteStatus) {
+
+      // update the city collection to delete this state id
+      const stateUpdateStatus = await StateModel.updateMany({
+        city: {
+          $in: cityId
+        }
+      },
+      {
+        $pull: {
+          city: cityId
+        }
+      }
+    );
+
+      // update the area collection to delete this state id
+      const areaUpdateStatus = await AreaModel.updateMany({city: cityId}, {
+        city: null,
+        active: false
+      });
+
+      // update the community collection to delete this state id
+      const communtiyUpdateStatus = await CommunityModel.updateMany({city: cityId}, {
+        state: null,
+        active: false
+      });
+
+      console.log("stateUpdateStatus:", stateUpdateStatus)
+      console.log("areaUpdateStatus:", areaUpdateStatus)
+      console.log("communtiyUpdateStatus:", communtiyUpdateStatus)
+      // try to check those cities, areas, and communities has update or not
+      if(stateUpdateStatus && communtiyUpdateStatus && areaUpdateStatus) {
+        res.status(200).json({
+          msg: "The city has deleted successfully"
+        })
+      } else {
+        res.status(500).json({
+          errors: {
+            msg: "There was an server side error"
+          }
+        })
+      }
+
+    } else {
+      res.status(500).json({
+        errors: {
+          msg: "There was an server side error"
+        }
+      })
+    }
+  } catch(err) {
+    console.log(err.message)
+  }
+}
+
+module.exports = deleteCity;
--- server/routers/cityRouter.js
+++ server/routers/cityRouter.js
@@ -4,6 +4,7 @@ const express = require("express");
 // internal controllers imported
 const addCity = require("../controllers/cityController/addCity");
 const updateCity = require("../controllers/cityController/updateCity");
+const deleteCity = require("../controllers/cityController/deleteCity");
 // const updateState = require("../controllers/stateController/updateState");
 // const deleteState = require("../controllers/stateController/deleteState");
 
@@ -14,6 +15,6 @@ const router = express.Router();
 // route controller
 router.post('/add', addCity);
 router.put('/update', updateCity);
-// router.delete('/delete', deleteState);
+router.delete('/delete', deleteCity);
 
 module.exports = router;
