0aa0377faef5406e720feb02b42be976a3fa9a78
Apon Shekh <aponshekh420@gmail.com>
2024-08-10 02:51:31 +0600

    try to deploy the backend

--- server/index.js
+++ server/index.js
@@ -70,7 +70,7 @@ app.use('/api/blog', blogRouter);
 app.use('/api/user', userRouter)
 
 // Server listening
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, ()=> {
   console.log(`The server has started at ${PORT}`)
