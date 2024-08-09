e1eccb9559b921f41ba4fe364a46b8ec8741dc8b
Apon Shekh <aponshekh420@gmail.com>
2024-08-10 00:56:24 +0600

    made change for deployment

--- client/src/components/common/DeleteModal.js
+++ client/src/components/common/DeleteModal.js
@@ -9,8 +9,7 @@ const DeleteModal = ({deleteHanlder, item, subject}) => {
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
-              Are you sure you want to delete the <b>"{item.name || item.title}"</b> {subject}?
-              {/* eslint-disable-line react/no-unescaped-entities */}
+              Are you sure you want to delete the <b>&quot;{item.name || item.title}&quot;</b> {subject}?
             </div>
             <div className="modal-footer">
               <button type="button" className="btn btn-outline-dark" data-bs-dismiss="modal">Close</button>
