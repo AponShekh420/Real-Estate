0d3ff3197325a16cb62912b17282a9df6b1a3044
Apon Shekh <aponshekh420@gmail.com>
2024-08-26 18:09:49 +0600

    the blogs and summary loading has made responsiveness

--- client/src/components/blogs/BlogsListLoader.js
+++ client/src/components/blogs/BlogsListLoader.js
@@ -4,43 +4,21 @@ const ContentLoader = dynamic(() => import('react-content-loader'), { ssr: false
 
 const BlogsListLoader = () => {
   return (
-    <div className='h-100 w-100 row'>
-      <div className={`col-sm-6 col-lg-4`}>
-        <ContentLoader viewBox="0 0 386 271" height={271} width={386} backgroundColor="#dee2e6"
-        foregroundColor="#ecebeb">
-          <rect x="3" y="3" rx="10" ry="10" width="270" height="220" />
-          <rect x="4" y="256" rx="0" ry="20" width="292" height="20" />
-          <rect x="4" y="280" rx="0" ry="0" width="239" height="20" />
-          <rect x="4" y="304" rx="0" ry="0" width="274" height="20" />
-        </ContentLoader>
-      </div>
-      <div className={`col-sm-6 col-lg-4`}>
-        <ContentLoader viewBox="0 0 390 330" height={330} width={390} backgroundColor="#dee2e6"
-        foregroundColor="#ecebeb">
-          <rect x="3" y="3" rx="10" ry="10" width="382" height="248" />
-          <rect x="4" y="256" rx="0" ry="20" width="292" height="20" />
-          <rect x="4" y="280" rx="0" ry="0" width="239" height="20" />
-          <rect x="4" y="304" rx="0" ry="0" width="274" height="20" />
-        </ContentLoader>
-      </div>
-      <div className={`col-sm-6 col-lg-4`}>
-        <ContentLoader viewBox="0 0 390 330" height={330} width={390} backgroundColor="#dee2e6"
-        foregroundColor="#ecebeb">
-          <rect x="3" y="3" rx="10" ry="10" width="382" height="248" />
-          <rect x="4" y="256" rx="0" ry="20" width="292" height="20" />
-          <rect x="4" y="280" rx="0" ry="0" width="239" height="20" />
-          <rect x="4" y="304" rx="0" ry="0" width="274" height="20" />
-        </ContentLoader>
-      </div>
-      <div className={`col-sm-6 col-lg-4`}>
-        <ContentLoader viewBox="0 0 390 330" height={330} width={390} backgroundColor="#dee2e6"
-        foregroundColor="#ecebeb">
-          <rect x="3" y="3" rx="10" ry="10" width="382" height="248" />
-          <rect x="4" y="256" rx="0" ry="20" width="292" height="20" />
-          <rect x="4" y="280" rx="0" ry="0" width="239" height="20" />
-          <rect x="4" y="304" rx="0" ry="0" width="274" height="20" />
-        </ContentLoader>
-      </div>
+    <div className="row">
+      {[...Array(10)].map((_, index) => (
+        <div key={index} className="col-12 col-sm-6 col-md-4 mb-4">
+          <ContentLoader 
+            viewBox="0 0 390 330"
+            style={{ width: '100%', height: 'auto' }}
+            backgroundColor="#dee2e6"
+            foregroundColor="#ecebeb"
+          >
+            <rect x="3" y="3" rx="10" ry="10" width="100%" height="70%" />
+            <rect x="4" y="246" rx="0" ry="20" width="75%" height="8%" />
+            <rect x="4" y="280" rx="0" ry="0" width="60%" height="8%" />
+          </ContentLoader>
+        </div>
+      ))}
     </div>
   )
 }
--- client/src/components/summary/CommunitiesListLoader.js
+++ client/src/components/summary/CommunitiesListLoader.js
@@ -1,103 +1,29 @@
-import React from 'react'
+import React from 'react';
 import dynamic from 'next/dynamic';
+
 const ContentLoader = dynamic(() => import('react-content-loader'), { ssr: false });
 
 const CommunitiesListLoader = () => {
   return (
-    <div className='h-100 w-100 row'>
-      <div className={`col-sm-6 col-lg-6`}>
-        <ContentLoader viewBox="0 0 390 330" height={330} width={390} backgroundColor="#dee2e6"
-        foregroundColor="#ecebeb">
-          <rect x="3" y="3" rx="10" ry="10" width="382" height="248" />
-          <rect x="4" y="256" rx="0" ry="20" width="292" height="20" />
-          <rect x="4" y="280" rx="0" ry="0" width="239" height="20" />
-          <rect x="4" y="304" rx="0" ry="0" width="274" height="20" />
-        </ContentLoader>
-      </div>
-      <div className={`col-sm-6 col-lg-6`}>
-        <ContentLoader viewBox="0 0 390 330" height={330} width={390} backgroundColor="#dee2e6"
-        foregroundColor="#ecebeb">
-          <rect x="3" y="3" rx="10" ry="10" width="382" height="248" />
-          <rect x="4" y="256" rx="0" ry="20" width="292" height="20" />
-          <rect x="4" y="280" rx="0" ry="0" width="239" height="20" />
-          <rect x="4" y="304" rx="0" ry="0" width="274" height="20" />
-        </ContentLoader>
-      </div>
-      <div className={`col-sm-6 col-lg-6`}>
-        <ContentLoader viewBox="0 0 390 330" height={330} width={390} backgroundColor="#dee2e6"
-        foregroundColor="#ecebeb">
-          <rect x="3" y="3" rx="10" ry="10" width="382" height="248" />
-          <rect x="4" y="256" rx="0" ry="20" width="292" height="20" />
-          <rect x="4" y="280" rx="0" ry="0" width="239" height="20" />
-          <rect x="4" y="304" rx="0" ry="0" width="274" height="20" />
-        </ContentLoader>
-      </div>
-      <div className={`col-sm-6 col-lg-6`}>
-        <ContentLoader viewBox="0 0 390 330" height={330} width={390} backgroundColor="#dee2e6"
-        foregroundColor="#ecebeb">
-          <rect x="3" y="3" rx="10" ry="10" width="382" height="248" />
-          <rect x="4" y="256" rx="0" ry="20" width="292" height="20" />
-          <rect x="4" y="280" rx="0" ry="0" width="239" height="20" />
-          <rect x="4" y="304" rx="0" ry="0" width="274" height="20" />
-        </ContentLoader>
-      </div>
-      <div className={`col-sm-6 col-lg-6`}>
-        <ContentLoader viewBox="0 0 390 330" height={330} width={390} backgroundColor="#dee2e6"
-        foregroundColor="#ecebeb">
-          <rect x="3" y="3" rx="10" ry="10" width="382" height="248" />
-          <rect x="4" y="256" rx="0" ry="20" width="292" height="20" />
-          <rect x="4" y="280" rx="0" ry="0" width="239" height="20" />
-          <rect x="4" y="304" rx="0" ry="0" width="274" height="20" />
-        </ContentLoader>
-      </div>
-      <div className={`col-sm-6 col-lg-6`}>
-        <ContentLoader viewBox="0 0 390 330" height={330} width={390} backgroundColor="#dee2e6"
-        foregroundColor="#ecebeb">
-          <rect x="3" y="3" rx="10" ry="10" width="382" height="248" />
-          <rect x="4" y="256" rx="0" ry="20" width="292" height="20" />
-          <rect x="4" y="280" rx="0" ry="0" width="239" height="20" />
-          <rect x="4" y="304" rx="0" ry="0" width="274" height="20" />
-        </ContentLoader>
-      </div>
-      <div className={`col-sm-6 col-lg-6`}>
-        <ContentLoader viewBox="0 0 390 330" height={330} width={390} backgroundColor="#dee2e6"
-        foregroundColor="#ecebeb">
-          <rect x="3" y="3" rx="10" ry="10" width="382" height="248" />
-          <rect x="4" y="256" rx="0" ry="20" width="292" height="20" />
-          <rect x="4" y="280" rx="0" ry="0" width="239" height="20" />
-          <rect x="4" y="304" rx="0" ry="0" width="274" height="20" />
-        </ContentLoader>
-      </div>
-      <div className={`col-sm-6 col-lg-6`}>
-        <ContentLoader viewBox="0 0 390 330" height={330} width={390} backgroundColor="#dee2e6"
-        foregroundColor="#ecebeb">
-          <rect x="3" y="3" rx="10" ry="10" width="382" height="248" />
-          <rect x="4" y="256" rx="0" ry="20" width="292" height="20" />
-          <rect x="4" y="280" rx="0" ry="0" width="239" height="20" />
-          <rect x="4" y="304" rx="0" ry="0" width="274" height="20" />
-        </ContentLoader>
-      </div>
-      <div className={`col-sm-6 col-lg-6`}>
-        <ContentLoader viewBox="0 0 390 330" height={330} width={390} backgroundColor="#dee2e6"
-        foregroundColor="#ecebeb">
-          <rect x="3" y="3" rx="10" ry="10" width="382" height="248" />
-          <rect x="4" y="256" rx="0" ry="20" width="292" height="20" />
-          <rect x="4" y="280" rx="0" ry="0" width="239" height="20" />
-          <rect x="4" y="304" rx="0" ry="0" width="274" height="20" />
-        </ContentLoader>
-      </div>
-      <div className={`col-sm-6 col-lg-6`}>
-        <ContentLoader viewBox="0 0 390 330" height={330} width={390} backgroundColor="#dee2e6"
-        foregroundColor="#ecebeb">
-          <rect x="3" y="3" rx="10" ry="10" width="382" height="248" />
-          <rect x="4" y="256" rx="0" ry="20" width="292" height="20" />
-          <rect x="4" y="280" rx="0" ry="0" width="239" height="20" />
-          <rect x="4" y="304" rx="0" ry="0" width="274" height="20" />
-        </ContentLoader>
-      </div>
+    <div className="h-100 w-100 row">
+      {[...Array(9)].map((_, index) => (
+        <div key={index} className="col-12 col-sm-6 col-md-6 mt25">
+          <ContentLoader 
+            viewBox="0 0 390 330" 
+            height="100%" 
+            width="100%" 
+            backgroundColor="#dee2e6" 
+            foregroundColor="#ecebeb"
+          >
+            <rect x="3" y="3" rx="10" ry="10" width="100%" height="70%" />
+            <rect x="4" y="240" rx="0" ry="20" width="75%" height="6%" />
+            <rect x="4" y="265" rx="0" ry="0" width="60%" height="6%" />
+            <rect x="4" y="290" rx="0" ry="0" width="70%" height="6%" />
+          </ContentLoader>
+        </div>
+      ))}
     </div>
-  )
-}
-
+  );
+};
 
 export default CommunitiesListLoader;
