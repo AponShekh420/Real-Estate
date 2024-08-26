0d26cbc019825812c12b9f7de4b596f38f48224d
Apon Shekh <aponshekh420@gmail.com>
2024-08-27 00:53:31 +0600

    has done to display latest blog posts on blogs page

--- client/src/components/blogs/sidebar/LatestPost.js
+++ client/src/components/blogs/sidebar/LatestPost.js
@@ -1,28 +1,33 @@
-import { posts } from "@/data/blogs";
+import TimeManager from "@/components/single-community/common/TimeManager";
+import getSpecificBlogs from "@/lib/getSpecificBlogs";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
-const LatestPost = () => {
-  
-
+const LatestPost = async () => {
+  const data = await getSpecificBlogs(0, 3, true, null, null, null);
+  console.log(data)
   return (
     <div className="sidebar-widget mb30">
       <h6 className="widget-title">Latest Posts</h6>
-      {posts.map((post, index) => (
+      {data?.map((post, index) => (
         <div
           className="list-news-style d-flex align-items-center mt20 mb20"
           key={index}
         >
           <div className="news-img flex-shrink-0">
-            <Image width={90} height={80} src={post.image} alt="blog" />
+            <Image 
+              width={90} 
+              height={80} 
+              alt="blog" 
+              src={`${process.env.NEXT_PUBLIC_BACKEND_API}/assets/blogs/${post.img}`}
+            />
           </div>
           <div className="news-content flex-shrink-1 ms-3">
             <p className="new-text mb0 fz14">
-              <Link href={`/blogs/${post.id}`}>{post.content}</Link>
+              <Link href={`/blog/${post.slug}`}>{post.title}</Link>
             </p>
             <a className="body-light-color" href="#">
-              {post.date.day}{' '}{post.date.month}, {' '}{post.date.year}
+              <TimeManager data={post}/>
             </a>
           </div>
         </div>
---
+++ client/src/lib/getSpecificBlogs.js
@@ -0,0 +1,30 @@
+const getSpecificBlogs = async (limitStart, limitEnd, active, catagory, subcatagory, titleSearch) => {
+
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/api/blog/get-by-filter`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        titleSearch,
+        catagoryId: catagory?._id,
+        subcatagoryId: subcatagory?._id,
+        active,
+        limitStart: limitStart,
+        limitEnd: limitEnd,
+      })
+    })
+    const dataRes = await res.json();
+    if(dataRes.msg) {
+      return dataRes?.data
+    } else {
+      return;
+    }
+  } catch(err) {
+    console.log(err.message)
+  }
+}
+
+export default getSpecificBlogs;
