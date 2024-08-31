4679ead295565af20ef2a138cc4ef7eeee7428c7
Apon Shekh <aponshekh420@gmail.com>
2024-09-01 01:03:56 +0600

    change the loading system for blogs page siderbar catagories

--- client/src/components/blogs/sidebar/CatagoriesList.js
+++ client/src/components/blogs/sidebar/CatagoriesList.js
@@ -43,39 +43,39 @@ const CatagoriesList = () => {
 
   return (
     <div className="w-100">
-      {loading ? (
-        <ContentLoaderWrapper/>
-      ) : (
       <div className="sidebar-widget mb30">
         <h6 className="widget-title">Catagories</h6>
-        <ul className="w-100 list-unstyled">
-          {catagoriesData.map((catagory, catagoryIndex)=> (
-            <li className={`${path.split('/')[2] === catagory.slug ? "text-danger": ""}`} key={catagoryIndex}>
-              <Link href={`/blogs/${catagory.slug}`}>
-                <div className="d-flex justify-content-between align-items-center">
-                  <p className={`text-capitalize m-0 ${path.split('/')[2] === catagory.slug ? "text-danger": ""}`}>{catagory.name} ({catagory?.blogs?.length})</p>
-                  {path.split('/')[2] !== catagory.slug ? <IoIosArrowDown className="p-0"/> : <IoIosArrowUp className="p-0 text-danger"/> }
-                </div>
-              </Link>
-              {/* fist chiled */}
-              <ul className="w-90 list-unstyled ml10" style={{marginTop: "2px", height: path.split('/')[2] === catagory.slug ? "100%": "0", overflow: path.split('/')[2] === catagory.slug ? "visible": "hidden" }}> {/**height: 0, overflow: "hidden" */}
-                {catagory?.subcatagory?.map((eachSubcatagory, cityIndex)=> {
-                  return (
-                    <li className={`${path.split('/')[3] === eachSubcatagory.slug ? "text-danger": ""}`} key={cityIndex}>
-                      <Link href={`/blogs/${catagory.slug}/${eachSubcatagory.slug}`}>
-                        <div className="d-flex justify-content-between align-items-center">
-                          <p className={`text-capitalize m-0 ${path.split('/')[3] === eachSubcatagory.slug ? "text-danger": ""}`}>{eachSubcatagory.name} ({eachSubcatagory.blogs.length})</p>
-                        </div>
-                      </Link>
-                    </li>
-                  )
-                })}
-              </ul>
-            </li>
-          ))}
-        </ul>
+        {loading ? (
+          <ContentLoaderWrapper/>
+        ) : (
+          <ul className="w-100 list-unstyled">
+            {catagoriesData.map((catagory, catagoryIndex)=> (
+              <li className={`${path.split('/')[2] === catagory.slug ? "text-danger": ""}`} key={catagoryIndex}>
+                <Link href={`/blogs/${catagory.slug}`}>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <p className={`text-capitalize m-0 ${path.split('/')[2] === catagory.slug ? "text-danger": ""}`}>{catagory.name} ({catagory?.blogs?.length})</p>
+                    {path.split('/')[2] !== catagory.slug ? <IoIosArrowDown className="p-0"/> : <IoIosArrowUp className="p-0 text-danger"/> }
+                  </div>
+                </Link>
+                {/* fist chiled */}
+                <ul className="w-90 list-unstyled ml10" style={{marginTop: "2px", height: path.split('/')[2] === catagory.slug ? "100%": "0", overflow: path.split('/')[2] === catagory.slug ? "visible": "hidden" }}> {/**height: 0, overflow: "hidden" */}
+                  {catagory?.subcatagory?.map((eachSubcatagory, cityIndex)=> {
+                    return (
+                      <li className={`${path.split('/')[3] === eachSubcatagory.slug ? "text-danger": ""}`} key={cityIndex}>
+                        <Link href={`/blogs/${catagory.slug}/${eachSubcatagory.slug}`}>
+                          <div className="d-flex justify-content-between align-items-center">
+                            <p className={`text-capitalize m-0 ${path.split('/')[3] === eachSubcatagory.slug ? "text-danger": ""}`}>{eachSubcatagory.name} ({eachSubcatagory.blogs.length})</p>
+                          </div>
+                        </Link>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
-      )}
     </div>
   );
 };
