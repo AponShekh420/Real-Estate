13eaae79b76edf185a25f8eed11ce7a712e30856
Apon Shekh <aponshekh420@gmail.com>
2024-09-04 02:29:17 +0600

    the avatar work is totally completed

---
+++ client/src/app/dashboard/users/page.js
@@ -0,0 +1,66 @@
+import DashboardHeader from "@/components/common/DashboardHeader";
+import MobileMenu from "@/components/common/mobile-menu";
+import Footer from "@/components/dashboard/Footer";
+import SidebarDashboard from "@/components/dashboard/SidebarDashboard";
+import DboardMobileNavigation from "@/components/dashboard/DboardMobileNavigation";
+import Container from "@/components/dashboard/dashboard-blogs/Container";
+import { redirect } from "next/navigation";
+import store from "@/redux/store";
+import { getSession } from "@/lib/authLib";
+
+export const metadata = {
+  title: "Dashboard Properties || Homez - Real Estate NextJS Template",
+};
+
+const DashboardMyBlogs = async () => {
+  const user = await getSession();
+
+  if(user.role == "admin" || user.role == "contributor") {
+    // nothing
+  } else {
+    redirect("/")
+  }
+
+
+  return (
+    <>
+      {/* Main Header Nav */}
+      <DashboardHeader />
+      {/* End Main Header Nav */}
+
+      {/* Mobile Nav  */}
+      <MobileMenu />
+      {/* End Mobile Nav  */}
+
+      {/* dashboard_content_wrapper */}
+      <div className="dashboard_content_wrapper">
+        <div className="dashboard dashboard_wrapper pr30 pr0-xl">
+          <SidebarDashboard user={user}/>
+          {/* End .dashboard__sidebar */}
+
+          <div className="dashboard__main pl0-md">
+            <div className="dashboard__content bgc-f7">
+              <div className="row pb40">
+                <div className="col-lg-12">
+                  <DboardMobileNavigation />
+                </div>
+                {/* End .col-12 */}
+              </div>
+              {/* End .row */}
+
+              <Container/>
+              {/* End .row */}
+            </div>
+            {/* End .dashboard__content */}
+
+            <Footer />
+          </div>
+          {/* End .dashboard__main */}
+        </div>
+      </div>
+      {/* dashboard_content_wrapper */}
+    </>
+  );
+};
+
+export default DashboardMyBlogs;
--- client/src/components/common/DashboardHeader.js
+++ client/src/components/common/DashboardHeader.js
@@ -104,14 +104,14 @@ const DashboardHeader = () => {
                     <li className=" user_setting">
                       <div className="dropdown">
                         <a className="btn" href="#" data-bs-toggle="dropdown">
-                        <Image
-                          width={32}
-                          height={32}
-                          style={{objectFit: "cover"}}
-                          className="rounded-circle"
-                          src={userInfo?.avatar?.split("/")[2] !== "lh3.googleusercontent.com" ? `${process.env.NEXT_PUBLIC_BACKEND_API}/assets/users/${userInfo?.avatar}` : userInfo?.avatar || "/images/user_avatar.png"}
-                          alt={userInfo?.firstName + " " + userInfo?.lastName}
-                        />
+                         <Image
+                            width={44}
+                            height={44}
+                            style={{objectFit: "cover"}}
+                            className="rounded-circle"
+                            src={userInfo?.avatar?.split("/")[2] !== "lh3.googleusercontent.com" ? userInfo?.avatar ? `${process.env.NEXT_PUBLIC_BACKEND_API}/assets/users/${userInfo?.avatar}` : "/images/user_avatar.png" : userInfo?.avatar}
+                            alt={userInfo?.firstName + " " + userInfo?.lastName}
+                          />
                         </a>
                         <div className="dropdown-menu">
                           {loading ? (
@@ -128,7 +128,7 @@ const DashboardHeader = () => {
                                         height={50}
                                         style={{objectFit: "cover"}}
                                         className="rounded-circle"
-                                        src={userInfo?.avatar?.split("/")[2] !== "lh3.googleusercontent.com" ? `${process.env.NEXT_PUBLIC_BACKEND_API}/assets/users/${userInfo?.avatar}` : userInfo?.avatar || "/images/user_avatar.png"}
+                                        src={userInfo?.avatar?.split("/")[2] !== "lh3.googleusercontent.com" ? userInfo?.avatar ? `${process.env.NEXT_PUBLIC_BACKEND_API}/assets/users/${userInfo?.avatar}` : "/images/user_avatar.png" : userInfo?.avatar}
                                         alt={userInfo?.firstName + " " + userInfo?.lastName}
                                       />
                                       <div className="meta d-sm-flex justify-content-sm-between align-items-center">
