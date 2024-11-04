b16c7bbefa44bc4b3e8cf549188b6e3d200fac34
Apon Shekh <aponshekh420@gmail.com>
2024-11-04 15:08:45 +0600

    The advance search problem has fixed

--- client/src/components/summary/sidebar/ListItem.js
+++ client/src/components/summary/sidebar/ListItem.js
@@ -9,31 +9,31 @@ const ListItem = ({parent, subElement, parentPathNumber, childrenPathNumber}) =>
   const path = usePathname();
 
   return (
-    <li className={`${path.split('/')[parentPathNumber] === parent.slug ? "text-danger": ""}`}>
-      <Link href={area ? `/summary/${state.slug}/${parent.slug}`
-      : `/summary/${parent.slug}`
+    <li className={`${path.split('/')[parentPathNumber] === parent?.slug ? "text-danger": ""}`}>
+      <Link href={area ? `/summary/${state?.slug}/${parent?.slug}`
+      : `/summary/${parent?.slug}`
     }>
         <div className="d-flex justify-content-between align-items-center">
-          <p className={`text-capitalize m-0 ${path.split('/')[parentPathNumber] === parent.slug ? "text-danger": ""}`}><b>{parent.name} ({parent.community.length})</b></p>
-          {path.split('/')[parentPathNumber] !== parent.slug ? <IoIosArrowDown className="p-0"/> : <IoIosArrowUp className="p-0 text-danger"/> }
+          <p className={`text-capitalize m-0 ${path.split('/')[parentPathNumber] === parent?.slug ? "text-danger": ""}`}><b>{parent?.name} ({parent?.community?.length})</b></p>
+          {path.split('/')[parentPathNumber] !== parent?.slug ? <IoIosArrowDown className="p-0"/> : <IoIosArrowUp className="p-0 text-danger"/> }
         </div>
       </Link>
       {/* fist chiled */}
-      <ul className="w-90 list-unstyled ml10" style={{marginTop: "2px", height: path.split('/')[parentPathNumber] === parent.slug ? "100%": "0", overflow: path.split('/')[childrenPathNumber] === parent.slug ? "visible": "hidden" }}> {/**height: 0, overflow: "hidden" */}
+      <ul className="w-90 list-unstyled ml10" style={{marginTop: "2px", height: path.split('/')[parentPathNumber] === parent?.slug ? "100%": "0", overflow: path.split('/')[childrenPathNumber] === parent?.slug ? "visible": "hidden" }}> {/**height: 0, overflow: "hidden" */}
         {subElement?.map((eachElement, eachElementIndex)=> {
           if(eachElement?.active) {
             return (
-              <li className={`${path.split('/')[childrenPathNumber] === eachElement.slug ? "text-danger": ""}`} key={eachElementIndex}>
+              <li className={`${path.split('/')[childrenPathNumber] === eachElement?.slug ? "text-danger": ""}`} key={eachElementIndex}>
                 <Link 
                   href={
                     area ? 
-                      `/summary/${state.slug}/${area.slug}/${eachElement.slug}` :
-                    state ? `/summary/${state.slug}/${eachElement.slug}` :
-                      `/summary/${state.slug}/`
+                      `/summary/${state?.slug}/${area?.slug}/${eachElement?.slug}` :
+                    state ? `/summary/${state?.slug}/${eachElement?.slug}` :
+                      `/summary/${state?.slug}/`
                     }>
                     <div className="d-flex justify-content-between align-items-center">
-                      <p className={`text-capitalize m-0 ${path.split('/')[childrenPathNumber] === eachElement.slug ? "text-danger": ""}`}>{eachElement.name} ({eachElement.community.length})</p>
-                      {path.split('/')[childrenPathNumber] !== eachElement.slug ? <IoIosArrowDown className="p-0"/> : <IoIosArrowUp className="p-0 text-danger"/> }
+                      <p className={`text-capitalize m-0 ${path.split('/')[childrenPathNumber] === eachElement?.slug ? "text-danger": ""}`}>{eachElement?.name} ({eachElement?.community?.length})</p>
+                      {path.split('/')[childrenPathNumber] !== eachElement?.slug ? <IoIosArrowDown className="p-0"/> : <IoIosArrowUp className="p-0 text-danger"/> }
                     </div>
                 </Link>
                 {/* second child */}
