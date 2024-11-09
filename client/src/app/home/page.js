11ebfa56e66b37f083cc6a373ab95259b4493599
Apon Shekh <aponshekh420@gmail.com>
2024-11-10 01:09:55 +0600

    The environment variable has changed again to fix the home page specific category

--- client/src/app/home/page.js
+++ client/src/app/home/page.js
@@ -84,8 +84,8 @@ const Home = () => {
           <div className="main-title2">
             <h2 className="title">Useful Information for 55 and Over Shoppers</h2>
           </div>
-          <RecentBlogs blogsCatagory={null} total={6} dateStatus={true} notCatagory={{_id: process.env.SPECIFIC_CETEGORY_HOME}}/>
-          <RecentBlogs blogsCatagory={{_id: process.env.SPECIFIC_CETEGORY_HOME}} total={3} dateStatus={false} notCatagory={""}/>
+          <RecentBlogs blogsCatagory={null} total={6} dateStatus={true} notCatagory={process.env.NEXT_PUBLIC_SPECIFIC_CETEGORY_HOME}/>
+          <RecentBlogs blogsCatagory={process.env.NEXT_PUBLIC_SPECIFIC_CETEGORY_HOME} total={3} dateStatus={false} notCatagory={""}/>
         </div>
       </section>
 
